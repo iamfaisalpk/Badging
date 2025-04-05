d125984aabfeef21f531ab81907155ec3e749931
iamfaisalpk <iamfaizy123@gmail.com>
2025-04-05 16:55:51 +0530

    third comt

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import FetchUser from "./assets/FetchUser"
+import Login from "./assets/Userss/Login"
+import Signup from "./assets/Userss/Signup"
 
 
 const App = () => {
   return (
     <>
-      {/* <Error/> */}
-      {/* <div className="p-4 max-w-md mx-auto">
-      <h1 className="text-xl font-bold mb-4">Todo List</h1>
-      <TodoInput/>
-      <TodoList /> 
-      </div> */}
-      {/* <Stopwatch/> */}
-      {/* <ProductDisplay/>
-      <ProductSelector/> */}
-      {/* <div>
-        <h2>Reusable</h2>
-        <Reusable
-        label="enter a password"
-        placeholder="type here.."
-        />
-
-        <Reusable
-        label="confirm password"
-        placeholder="Re-type password"/>
-      </div> */}
-{/* 
-      <Button label="click Me" onClick={()=> alert("Button Clicked")}/>
-        <Button label="Delete" onClick={()=> alert("delet the content")} className="bg-red-500 ml-2"/> */}
-        {/* <Signup/>
-        <Login/> */}
-        <FetchUser/>
-        {/* <orderFood/> */}
+      <Signup/>
+      <Login/>
     </>
   )
 }
